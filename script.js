c72120cc6a29b409c2a6986f88361bd1058d239f
Peter Sidlauskas <peter@bronze56k.com>
2023-02-28 17:32:58 -0500

    making commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const swup = new Swup();
+
+function onHover()
+{
+    document.getElementById('video1').src="thrash1.jpg";
+    document.getElementById('video2').src="fwiwr.jpg";
+}
+
+function offHover()
+{
+    document.getElementById('video1').src="thrash.jpg";
+    document.getElementById('video2').src="fwiw.jpg";
+}
